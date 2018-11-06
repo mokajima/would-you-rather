9592e5673b1b2d76e48a79a9c14f997556a743e3
mokajima <mokajima.85z@gmail.com>
2018-11-06 23:52:30 +0900

    feat: Add Data components

---
+++ src/components/Data.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+function Data(props) {
+  const { question, answer, option } = props
+
+  const className = option === answer
+    ? 'result__option option option--selected'
+    : 'result__option option'
+
+  const total = question.optionOne.votes.length + question.optionTwo.votes.length
+  const rate = 100 * question[option].votes.length / total
+
+  return (
+    <div className={className}>
+      <p>Would you rather {question[option].text}?</p>
+      <div className="chart">
+        <span className="chart__bar" style={{ width: rate + '%' }}></span>
+        <span className="chart__text" style={{ left: rate + '%' }}>{rate.toFixed(0)}%</span>
+      </div>
+      {option === answer
+        ? <p className="center">{question[option].votes.length} out of {total} votes (Your vote)</p>
+        : <p className="center">{question[option].votes.length} out of {total} votes</p>
+      }
+    </div>
+  )
+}
+
+function mapStateToProps({ authedUser, users, questions }, { id, option }) {
+  const question = questions[id]
+  const answer = users[authedUser].answers[id]
+
+  return {
+    question,
+    answer,
+    option
+  }
+}
+
+export default connect(mapStateToProps)(Data)
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,43 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import Data from './Data'
 
 function Result(props) {
-  const { optionOne, optionTwo } = props.question
-  const { answer } = props
-
-  const total = optionOne.votes.length + optionTwo.votes.length
+  const { id } = props
 
   return (
-    <div>
-      <span>Results:</span>
-      <div>
-        <span>Would you rather {optionOne.text}?</span>
-        <span>{Math.round(optionOne.votes.length / total * 1000) / 10}%</span>
-        <span>{optionOne.votes.length} out of {total} votes</span>
-        {'optionOne' === answer && (
-          <span>Your vote</span>
-        )}
-      </div>
-      <div>
-        <span>Would you rather {optionTwo.text}?</span>
-        <span>{Math.round(optionTwo.votes.length / total * 1000) / 10}%</span>
-        <span>{optionTwo.votes.length} out of {total} votes</span>
-        {'optionTwo' === answer && (
-          <span>Your vote</span>
-        )}
-      </div>
+    <div className="result">
+      <p className="result__title">Results:</p>
+      <Data id={id} option="optionOne" />
+      <Data id={id} option="optionTwo" />
     </div>
   )
 }
 
-function mapStateToProps({ authedUser, users, questions }, { id }) {
-  const question = questions[id]
-  const answer = users[authedUser].answers[id]
-
-  return {
-    question,
-    answer
-  }
-}
-
-export default connect(mapStateToProps)(Result)
+export default Result
