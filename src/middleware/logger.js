38f5cd0e449721c156142ffe363fc967028790ad
mokajima <mokajima.85z@gmail.com>
2019-01-14 15:43:25 +0900

    chore: Add comments to temporarily disable ESLint rules

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,9 +1,11 @@
 const logger = (store) => (next) => (action) => {
+  /* eslint-disable no-console, indent */
   console.group(action.type)
     console.log('The action: ', action)
     const returnValue = next(action)
     console.log('The new state: ', store.getState())
   console.groupEnd()
+  /* eslint-enable no-console, indent */
   return returnValue
 }
 
