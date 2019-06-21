361fd6f5398dd8bcffd6eec8b623e56fe615f897
mokajima <mokajima85z@gmail.com>
2019-06-21 14:41:38 +0900

    refactor: Refactor the ScoreCard

--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,44 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ScoreCard = ({ user }) => {
-  const answeredQuestions = Object.keys(user.answers).length
-  const createdQuestions = user.questions.length
-
-  return (
-    <div className="score-card">
-      <div className="score-card__col score-card__avatar">
-        <img
-          className="avatar avatar--md"
-          src={user.avatarURL}
-          alt=""
-        />
-      </div>
-      <div className="score-card__col">
-        <span className="score-card__name">{user.name}</span>
-        <table className="score-card__table">
-          <tbody>
-            <tr>
-              <th>Answered questions</th>
-              <td>{answeredQuestions}</td>
-            </tr>
-            <tr>
-              <th>Created questions</th>
-              <td>{createdQuestions}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <dl className="score-card__col score">
-        <dt className="score__title">Score</dt>
-        <dd className="score__data">{answeredQuestions + createdQuestions}</dd>
-      </dl>
+const ScoreCard = ({
+  user,
+  answeredQuestions,
+  createdQuestions,
+  score
+}) => (
+  <div className="score-card">
+    <div className="score-card__col score-card__avatar">
+      <img
+        className="avatar avatar--md"
+        src={user.avatarURL}
+        alt=""
+      />
     </div>
-  )
-}
+    <div className="score-card__col">
+      <span className="score-card__name">{user.name}</span>
+      <table className="score-card__table">
+        <tbody>
+          <tr>
+            <th>Answered questions</th>
+            <td>{answeredQuestions}</td>
+          </tr>
+          <tr>
+            <th>Created questions</th>
+            <td>{createdQuestions}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <dl className="score-card__col score">
+      <dt className="score__title">Score</dt>
+      <dd className="score__data">{score}</dd>
+    </dl>
+  </div>
+)
 
 ScoreCard.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
+  answeredQuestions: PropTypes.number.isRequired,
+  createdQuestions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired
 }
 
 export default ScoreCard
--- src/containers/ScoreCard.js
+++ src/containers/ScoreCard.js
@@ -1,8 +1,18 @@
 import { connect } from 'react-redux'
 import ScoreCard from '../components/ScoreCard'
 
-const mapStateToProps = ({ users }, { id }) => ({
-  user: users[id]
-})
+const mapStateToProps = ({ users }, { id }) => {
+  const user = users[id]
+  const answeredQuestions = Object.keys(user.answers).length
+  const createdQuestions = user.questions.length
+  const score = answeredQuestions + createdQuestions
+
+  return {
+    user,
+    answeredQuestions,
+    createdQuestions,
+    score
+  }
+}
 
 export default connect(mapStateToProps)(ScoreCard)
