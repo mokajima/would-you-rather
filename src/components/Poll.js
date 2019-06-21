65ce969604bb957f26b4d671df96dd3af5157abe
mokajima <mokajima85z@gmail.com>
2019-06-21 14:57:17 +0900

    refactor: Refactor the Poll component

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -10,33 +10,37 @@ const Poll = ({
   answer,
   id
 }) => {
-  return question
-    ? (
-      <div className="card">
-        <p className="card__title">{author.name} asks:</p>
-        <div className="card__content">
-          <div className="card__avatar">
-            <img
-              className="avatar avatar--lg"
-              src={author.avatarURL}
-              alt=""
-            />
-          </div>
-          {answer
-            ? <Result id={id} />
-            : <PollForm id={id} / >
-          }
-        </div>
-      </div>
-    )
-    : (
+  if (!question) {
+    return (
       <div>
         <div className="logo">
           <img src={logo} alt="" />
         </div>
-        <p className="center">Oops, the page cannot be found.</p>
+        <p className="center">
+          Oops, the page cannot be found.
+        </p>
       </div>
     )
+  }
+
+  return (
+    <div className="card">
+      <p className="card__title">{author.name} asks:</p>
+      <div className="card__content">
+        <div className="card__avatar">
+          <img
+            className="avatar avatar--lg"
+            src={author.avatarURL}
+            alt=""
+          />
+        </div>
+        {answer
+          ? <Result id={id} />
+          : <PollForm id={id} / >
+        }
+      </div>
+    </div>
+  )
 }
 
 Poll.propTypes = {
