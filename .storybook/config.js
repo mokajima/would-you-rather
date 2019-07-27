5ac13181568ae4ba65544a64d523f1a60af6e56a
mokajima <mokajima85z@gmail.com>
2019-07-27 16:38:06 +0900

    chore: Add StoryShots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withKnobs } from '@storybook/addon-knobs'
+import requireContext from 'require-context.macro'
 
 import '../src/index.css'
 
-const req = require.context('../src/components', true, /.(story|stories).tsx$/)
+const req = requireContext('../src/components', true, /.(story|stories).tsx$/)
 
 function loadStories() {
   addDecorator(withInfo)
