eb96fed120fad22c734d524f642895860f107b9e
mokajima <mokajima85z@gmail.com>
2019-08-01 22:55:55 +0900

    refactor: Pass watchers to fork

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,13 +1,13 @@
-import { all } from 'redux-saga/effects'
+import { all, fork } from 'redux-saga/effects'
 import { watchGetQuestions } from './questions'
 import { watchAddQuestion, watchAnswerQuestion } from './shared'
 import { watchGetUsers } from './users'
 
 export default function* rootSaga() {
   yield all([
-    watchGetQuestions(),
-    watchAddQuestion(),
-    watchAnswerQuestion(),
-    watchGetUsers()
+    fork(watchGetQuestions),
+    fork(watchAddQuestion),
+    fork(watchAnswerQuestion),
+    fork(watchGetUsers)
   ])
 }
