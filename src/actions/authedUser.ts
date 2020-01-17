50ed2a31f3269a552751cdf3c505f92e74c6547e
mokajima <mokajima85z@gmail.com>
2020-01-17 22:50:25 +0900

    style: Use arrow functions

--- src/actions/authedUser.ts
+++ src/actions/authedUser.ts
@@ -4,18 +4,14 @@ interface SetAuthedUserParams {
   id: string
 }
 
-export function setAuthedUser(params: SetAuthedUserParams) {
-  return {
-    type: ActionType.SET_AUTHED_USER as typeof ActionType.SET_AUTHED_USER,
-    payload: params
-  }
-}
+export const setAuthedUser = (params: SetAuthedUserParams) => ({
+  type: ActionType.SET_AUTHED_USER as typeof ActionType.SET_AUTHED_USER,
+  payload: params
+})
 
-export function removeAuthedUser() {
-  return {
-    type: ActionType.REMOVE_AUTHED_USER as typeof ActionType.REMOVE_AUTHED_USER
-  }
-}
+export const removeAuthedUser = () => ({
+  type: ActionType.REMOVE_AUTHED_USER as typeof ActionType.REMOVE_AUTHED_USER
+})
 
 export type authedUserAction =
   | ReturnType<typeof setAuthedUser>
